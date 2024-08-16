5367a6bc7e4f01f9d170c868202bb5514adc2bbb
Baenicha <98473820+Baenicha@users.noreply.github.com>
2024-08-16 12:59:24 +0200

    Update database configuration for JawsDB

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,15 +44,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', parse_url(env('JAWSDB_URL'), PHP_URL_HOST)),
+            'port' => env('DB_PORT', parse_url(env('JAWSDB_URL'), PHP_URL_PORT)),
+            'database' => env('DB_DATABASE', ltrim(parse_url(env('JAWSDB_URL'), PHP_URL_PATH), '/')),
+            'username' => env('DB_USERNAME', parse_url(env('JAWSDB_URL'), PHP_URL_USER)),
+            'password' => env('DB_PASSWORD', parse_url(env('JAWSDB_URL'), PHP_URL_PASS)),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
@@ -61,6 +60,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
